d5e2674cc213af869be0b4231ec42119e700fc55
Huntsman402548 <hunter@kidesign.com>
2024-12-24 21:20:10 -0800

    Initial commit

---
+++ src/main/java/net/hunter/modproject/ModProjectClient.java
@@ -0,0 +1,10 @@
+package net.hunter.modproject;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModProjectClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
